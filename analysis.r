239b31663fd6fbab01df247292f5b8eaf8525b89
Ryan Lee <ryanjlee22@gmail.com>
2021-07-26 15:20:06 -0400

    Add basic analysis file

---
+++ analysis.r
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(ggplot2)
+
+
+norway_data <- read_csv("/Users/ryanlee/Documents/Projects/carbon-emissions-data/raw/norway_table_emissions.csv")
+sweden_data <- read_csv("/Users/ryanlee/Documents/Projects/carbon-emissions-data/raw/sweden_table_emissions.csv")
+
+#Scatterplots
+ggplot(norway_data, aes(x = Year, y = Emissions)) + geom_point()
+ggplot(sweden_data, aes(x = Year, y = Emissions)) + geom_point()
+
+#Regressions of total emissions
+norway_total_emissions_regression = lm(formula = Emissions ~ Year, data = norway_data)
+sweden_total_emissions_regression = lm(formula = Emissions ~ Year, data = sweden_data)
+
+#Regression of emissions per GDP
+names(norway_data)[10] = 'gdp'
+sweden_gdp_emissions_regression = lm(formula = Emissions ~ Year / gdp, data = sweden_data)
+
+names(sweden_data)[10] = 'gdp'
+sweden_gdp_emissions_regression = lm(formula = Emissions ~ Year / gdp, data = sweden_data)
